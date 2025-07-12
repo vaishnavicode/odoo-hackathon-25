47d25898ba731b50cce12f366495391aed4a5feb
Janmejay Chatterjee <janmejay.c@iqm.com>
2025-07-12 11:35:33 +0530

    fix: Correct formatting in settings.py and update requirements.txt for consistency

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework_simplejwt',
-    'django_filters'
+    'django_filters',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders'
 
