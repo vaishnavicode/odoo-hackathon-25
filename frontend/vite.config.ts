b8b6d7079765a6d7c47d1d2aaf9725eecb980ab4
Janmejay Chatterjee <janmejay.c@iqm.com>
2025-07-12 11:01:25 +0530

    feature: Setup client side application boilerplate

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
+export default defineConfig({
+    plugins: [
+        tanstackRouter({
+            target: "react",
+            autoCodeSplitting: true,
+        }),
+        react(),
+        tailwindcss(),
+    ],
+});
